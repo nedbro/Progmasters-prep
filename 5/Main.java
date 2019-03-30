4eb0477d99293257e5e556fc54d7b16658a4b10b
nedbro <robertzkocsis@gmail.com>
2019-03-30 21:02:43 +0000

    Add the finished 5th project

--- 5/Main.java
+++ 5/Main.java
@@ -2,6 +2,13 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         
+        String text = input.nextLine();
+        int start = input.nextInt();
+        int end = input.nextInt();
+        
+        String substring = text.substring(start,end);
+        System.out.println(substring);
     }
 }
