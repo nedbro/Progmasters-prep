2a7f0e3e7a6e89a47b8bb7148dd37bfb74920577
nedbro <robertzkocsis@gmail.com>
2019-03-30 20:25:05 +0000

    Add finished 4th project

--- 4/Main.java
+++ 4/Main.java
@@ -2,6 +2,18 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
         
-    }
+        int arraySize = input.nextInt();
+        int[] array = new int[arraySize];
+        
+        for( int i = 0; i < arraySize; i++ ) {
+            int x = input.nextInt();
+            array[i] = x;
+        }
+        
+        for( int i = 0; i < arraySize; i++) {
+            System.out.println(array[i]);
+        }
+    } 
 }
