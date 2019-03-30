7e04f0f4599c0bcd74b183782a3b1ae69908507c
nedbro <robertzkocsis@gmail.com>
2019-03-30 21:30:09 +0000

    Add finished sixth project

--- 6/Main.java
+++ 6/Main.java
@@ -2,6 +2,21 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
         
+        String word = input.nextLine();
+        
+        String[] wordArray = word.split("");
+        String[] wordArrayBackwards = new String[wordArray.length];
+        
+        int helper = 0;
+        for(int i = (wordArray.length - 1); i > -1; i--){
+            wordArrayBackwards[helper] = wordArray[i];
+            helper++;
+        }
+        
+        if( Arrays.equals( wordArray, wordArrayBackwards ) ) {
+            System.out.println("Yes");
+        } else System.out.println("No");
     }
 }
