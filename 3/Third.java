660a9e2d8a5c9589eb8d09560ec4509566ecfbdc
nedbro <robertzkocsis@gmail.com>
2019-03-30 20:13:54 +0000

    Add finished third project

--- 3/Third.java
+++ 3/Third.java
@@ -2,6 +2,11 @@ import java.util.*;
 
 public class Third {
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
         
+        for( int i = 1; i < 11; i++ ){
+            System.out.println(n + " x " + i + " = " + n * i);
+        }
     }
 }
