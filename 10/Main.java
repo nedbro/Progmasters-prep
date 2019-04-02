2a7db8729fb3dc51b35afb9266b07bb1651ccfd9
nedbro <robertzkocsis@gmail.com>
2019-04-02 20:20:37 +0100

    Add finished 10th project

--- 10/Main.java
+++ 10/Main.java
@@ -2,6 +2,35 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        String firstString = input.nextLine();
+        String secondString = input.nextLine();
+        
+        int firstLength = firstString.length();
+        int secondLength = secondString.length();
+        
+        int max = 0;
+        String result = "";
+        
+        if (firstLength >= secondLength) max = firstLength;
+        else max = secondLength;
+        
+        for(int i = 0; i < max; i++) {
+            if ( (i + 1) > secondLength ) {
+                result += firstString.substring(i,firstLength);
+                break;
+            }
+             else if ( (i + 1) > firstLength ) {
+                result += secondString.substring(i,secondLength);
+                break;
+            } else {
+                result += secondString.substring(i, i+1);
+                result += firstString.substring(i,i+1);
+            }
+        }
+        
+        System.out.println(result);
         
     }
 }
