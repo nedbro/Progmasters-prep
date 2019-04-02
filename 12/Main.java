f70cd3976d5b384c8a090d43afafcd375bd3d54c
nedbro <robertzkocsis@gmail.com>
2019-04-02 21:17:31 +0100

    Add finished 12th project

--- 12/Main.java
+++ 12/Main.java
@@ -2,6 +2,22 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int size = input.nextInt();
+        
+        int ones = 0;
+        int fours = 0;
+        
+        for(int i = 0; i < size; i++) {
+            int x = input.nextInt();
+            
+            if(x == 1) ones++;
+            else if (x == 4) fours++;
+            
+        }
+        
+        if(ones < fours) System.out.println("true");
+        else System.out.println("false");
         
     }
 }
