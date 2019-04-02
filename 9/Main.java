a68f9f71a28eb061ed5e02fbe5f21c5bafa15569
nedbro <robertzkocsis@gmail.com>
2019-04-02 19:56:36 +0100

    Add finished 9th project

--- 9/Main.java
+++ 9/Main.java
@@ -1,6 +1,19 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String text = input.nextLine();
+        int number = input.nextInt();
         
+        int textLength = text.length();
+        String textSubString = text.substring(textLength - number , textLength);
+        
+        String output = "";
+        
+        for(int i = 0; i <= number; i++) {
+            output += textSubString;
+        }
+        
+        System.out.println(output);
     }
 }
